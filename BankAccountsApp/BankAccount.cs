b5993ba24bcf2cdeac79ac15dc2c8ceab95d219b
Rexzy9 <adelowotanw@gmail.com>
2025-12-02 08:24:28 +0100

    version 3

--- BankAccountsApp/BankAccount.cs
+++ BankAccountsApp/BankAccount.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace BankAccountsApp
 {
     public class BankAccount
-    {
+    {   
         public string Owner { get; set; }
         public Guid AccountNumber { get; set; }
         public decimal Balance { get; set; }
